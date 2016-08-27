0d9325bcfa849ca045094872ccb73528e78a0977
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2016-08-27 00:28:05 -0500

    Use resource `user.json` instead of `user`.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,21 +29,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user.as_json, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    respond_to do |format|
+      if @user.save
+       format.html { redirect_to @user, notice: 'User was successfully created.' }
+       format.json { render :show, status: :created, location: @user }
+      else
+       format.html { render :new }
+       format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
-
-    # respond_to do |format|
-    #   if @user.save
-    #    format.html { redirect_to @user, notice: 'User was successfully created.' }
-    #    format.json { render :show, status: :created, location: @user }
-    #   else
-    #    format.html { render :new }
-    #    format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   # PATCH/PUT /users/1
