e5c1f7213d2b951a5870dcd146791fb3e8e26174
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2016-08-27 12:53:19 -0500

    Redirect to dashboard after sending email.

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -10,6 +10,7 @@ class AppController < ApplicationController
     @driver = User.find params[:driver_id]
     @passenger = User.find params[:passenger_id]
     AppMailer.candidate_passenger_email(@driver, @passenger, params[:message]).deliver
+    redirect_to 'http://puber.xyz/dashboard.html'
   end
 
 end
