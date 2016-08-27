592de36ed9c698172cc96b38b6f4b4fbf5651d8e
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2016-08-27 10:54:06 -0500

    Recenter map, drop geolocation.

--- app/assets/javascripts/driver-passenger.js
+++ app/assets/javascripts/driver-passenger.js
@@ -6,25 +6,25 @@ var directionsService;
 function initMap() {
     mapOptions = {
         zoom: 15,
-        center: { lat: 23.6266557, lng: -102.5375005 },
+        center: { lat: 25.6702468, lng: -100.3778658 },
     };
     theMap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     directionsService = new google.maps.DirectionsService();
 
     // Try HTML5 geolocation
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            theMap.setCenter(pos);
-        }, function() {
-            alert('Error: The geolocation service failed.');
-        });
-    } else {
-        alert('Error: Your browser does not support geolocation.');
-    }
+    // if(navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(function(position) {
+    //         var pos = {
+    //             lat: position.coords.latitude,
+    //             lng: position.coords.longitude
+    //         };
+    //         theMap.setCenter(pos);
+    //     }, function() {
+    //         alert('Error: The geolocation service failed.');
+    //     });
+    // } else {
+    //     alert('Error: Your browser does not support geolocation.');
+    // }
 };
 
 function addMarker(location, map, title) {
